1bb3145f63fcb2c44e69906f56d3818bccbc1b20
Andrey <andrey.zolotin@gmail.com>
2019-05-20 09:18:23 +0200

    feat(data): add data loader

--- main.js
+++ main.js
@@ -2,3 +2,11 @@ var utm = "+proj=utm +zone=32";
 var wgs84 = "+proj=longlat +ellps=WGS84 +datum=WGS84 +no_defs";
 const coordinates = proj4(utm, wgs84, [473366.239, 5549510.9]);
 console.log(coordinates[1], coordinates[0]);
+
+d3.csv("data.csv").then(function(data) {
+    console.log(data[10]);
+});
+//
+// d3.dsvFormat(';')("data.csv", function(error, data){
+//     console.log(data[10]);
+// })
