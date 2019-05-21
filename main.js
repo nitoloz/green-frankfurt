247383880ade0a4428195e4386367beabcc3a12e
Andrey <andrey.zolotin@gmail.com>
2019-05-21 13:41:23 +0200

    feat(data): add points on the map

--- main.js
+++ main.js
@@ -1,12 +1,34 @@
-var utm = "+proj=utm +zone=32";
-var wgs84 = "+proj=longlat +ellps=WGS84 +datum=WGS84 +no_defs";
-const coordinates = proj4(utm, wgs84, [473366.239, 5549510.9]);
-console.log(coordinates[1], coordinates[0]);
+let utm = "+proj=utm +zone=32";
+let wgs84 = "+proj=longlat +ellps=WGS84 +datum=WGS84 +no_defs";
+let coordinates;
+// = proj4(utm, wgs84, [473366.239, 5549510.9]);
+// console.log(coordinates[1], coordinates[0]);
 
-d3.csv("data.csv").then(function(data) {
-    console.log(data[10]);
-});
-//
-// d3.dsvFormat(';')("data.csv", function(error, data){
-//     console.log(data[10]);
-// })
+function initMap() {
+    let myLatlng = new google.maps.LatLng(50.113651, 8.678511);
+    let mapOptions = {
+        zoom: 10,
+        center: myLatlng
+    };
+    let map = new google.maps.Map(document.getElementById("map"), mapOptions);
+
+    let processedData = [];
+    d3.csv("data.csv").then(data => {
+        processedData = data.map((d, i) => {
+            coordinates = proj4(utm, wgs84, [parseFloat(d.RECHTSWERT), parseFloat(d.HOCHWERT)]);
+            if (i < 1000) {
+
+                let marker = new google.maps.Marker({
+                    position: new google.maps.LatLng(coordinates[1], coordinates[0]),
+                    title: "Hello World!"
+                });
+                marker.setMap(map);
+            }
+
+            return {
+                d, ...{'latitude': coordinates[1], 'longitude': coordinates[0]}
+            };
+        });
+        console.log(processedData[10]);
+    });
+}
