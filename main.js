0880adaeb0c306ef2d453340e81dba83287db878
Andrey <andrey.zolotin@gmail.com>
2019-05-21 13:51:30 +0200

    feat(data): replace points with heatmap

--- main.js
+++ main.js
@@ -1,34 +1,27 @@
 let utm = "+proj=utm +zone=32";
 let wgs84 = "+proj=longlat +ellps=WGS84 +datum=WGS84 +no_defs";
 let coordinates;
-// = proj4(utm, wgs84, [473366.239, 5549510.9]);
-// console.log(coordinates[1], coordinates[0]);
 
 function initMap() {
-    let myLatlng = new google.maps.LatLng(50.113651, 8.678511);
+    let myLatlng = new google.maps.LatLng(50.128728, 8.667937);
     let mapOptions = {
-        zoom: 10,
+        zoom: 12.5,
         center: myLatlng
     };
     let map = new google.maps.Map(document.getElementById("map"), mapOptions);
-
+    let heatmap;
     let processedData = [];
     d3.csv("data.csv").then(data => {
         processedData = data.map((d, i) => {
             coordinates = proj4(utm, wgs84, [parseFloat(d.RECHTSWERT), parseFloat(d.HOCHWERT)]);
-            if (i < 1000) {
-
-                let marker = new google.maps.Marker({
-                    position: new google.maps.LatLng(coordinates[1], coordinates[0]),
-                    title: "Hello World!"
-                });
-                marker.setMap(map);
-            }
-
             return {
                 d, ...{'latitude': coordinates[1], 'longitude': coordinates[0]}
             };
         });
-        console.log(processedData[10]);
+        heatmap = new google.maps.visualization.HeatmapLayer({
+            data: processedData.map(d => new google.maps.LatLng(d.latitude, d.longitude)),
+            map: map
+        });
+        console.log('Data processed!');
     });
 }
