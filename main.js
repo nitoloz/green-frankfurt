a9d1108085b473f9c96a9d4c0e1dda333ad520a0
Andrey <andrey.zolotin@gmail.com>
2019-05-22 13:10:41 +0200

    feat(data): group trees by name

--- main.js
+++ main.js
@@ -15,9 +15,16 @@ function initMap() {
         processedData = data.map((d, i) => {
             coordinates = proj4(utm, wgs84, [parseFloat(d.RECHTSWERT), parseFloat(d.HOCHWERT)]);
             return {
-                d, ...{'latitude': coordinates[1], 'longitude': coordinates[0]}
+                ...d, ...{'latitude': coordinates[1], 'longitude': coordinates[0]}
             };
         });
+        const internalMap = new Map();
+        processedData.forEach(d => internalMap[d['Gattung/Art/Deutscher Name']] ? internalMap[d['Gattung/Art/Deutscher Name']]++ : internalMap[d['Gattung/Art/Deutscher Name']] = 1);
+        const values = Object.keys(internalMap).map((key) => {
+            return {value: key, count: internalMap[key]};
+        }).sort((a, b) => b.count - a.count);
+        console.log(values);
+
         heatmap = new google.maps.visualization.HeatmapLayer({
             data: processedData.map(d => new google.maps.LatLng(d.latitude, d.longitude)),
             map: map
