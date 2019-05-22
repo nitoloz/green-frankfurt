c3b5abcd4016d4f542b4fe04e12987a97c02025f
Andrey <andrey.zolotin@gmail.com>
2019-05-22 08:35:39 +0200

    feat(map): change default zoom

--- main.js
+++ main.js
@@ -5,7 +5,7 @@ let coordinates;
 function initMap() {
     let myLatlng = new google.maps.LatLng(50.128728, 8.667937);
     let mapOptions = {
-        zoom: 12.5,
+        zoom: 12.8,
         center: myLatlng
     };
     let map = new google.maps.Map(document.getElementById("map"), mapOptions);
