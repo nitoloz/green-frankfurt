78e1da1ca07298b12c846fda51a8e0f71f2996d3
Andrey <andrey.zolotin@gmail.com>
2019-05-23 13:32:21 +0200

    feat(map): initialize select before data is fetched

--- main.js
+++ main.js
@@ -10,6 +10,8 @@ function initMap() {
 
     let heatmap;
     let processedData = [];
+    appendMultiSelect([]);
+
     d3.csv("data.csv").then(data => {
         processedData = data.map((d, i) => {
             coordinates = proj4(utm, wgs84, [parseFloat(d.RECHTSWERT), parseFloat(d.HOCHWERT)]);
@@ -17,38 +19,38 @@ function initMap() {
         });
         const internalMap = new Map();
         processedData.forEach(d => internalMap[d['Gattung/Art/Deutscher Name']] ? internalMap[d['Gattung/Art/Deutscher Name']]++ : internalMap[d['Gattung/Art/Deutscher Name']] = 1);
-        // const values = Object.keys(internalMap).map((key) => {
-        //     return {value: key, count: internalMap[key]};
-        // }).sort((a, b) => b.count - a.count);
-        // appendMultiSelect(values);
-
-        const markers = processedData.map(function (location, i) {
-            return new google.maps.Marker({
-                position: {lat: location.lat, lng: location.lng},
-                icon: {
-                    path: google.maps.SymbolPath.CIRCLE,
-                    scale: 5,
-                    fillColor: "green",
-                    fillOpacity: 0.4,
-                    strokeWeight: 0.4
-                }
-            });
-        });
-        // Add a marker clusterer to manage the markers.
-        const markerCluster = new MarkerClusterer(map, markers,
-            {
-                imagePath: 'https://developers.google.com/maps/documentation/javascript/examples/markerclusterer/m',
-                minimumClusterSize: 25
-            });
-        // heatmap = new google.maps.visualization.HeatmapLayer({
-        //     data: processedData.map(d => new google.maps.LatLng(d.lat, d.lng)),
-        //     map: map
+        const values = Object.keys(internalMap).map((key) => {
+            return {value: key, count: internalMap[key]};
+        }).sort((a, b) => b.count - a.count);
+        $('select')[0].selectize.addOption(values);
+        // const markers = processedData.map(function (location, i) {
+        //     return new google.maps.Marker({
+        //         position: {lat: location.lat, lng: location.lng},
+        //         icon: {
+        //             path: google.maps.SymbolPath.CIRCLE,
+        //             scale: 5,
+        //             fillColor: "green",
+        //             fillOpacity: 0.4,
+        //             strokeWeight: 0.4
+        //         }
+        //     });
         // });
+        // // Add a marker clusterer to manage the markers.
+        // const markerCluster = new MarkerClusterer(map, markers,
+        //     {
+        //         imagePath: 'https://developers.google.com/maps/documentation/javascript/examples/markerclusterer/m',
+        //         minimumClusterSize: 25
+        //     });
+        heatmap = new google.maps.visualization.HeatmapLayer({
+            data: processedData.map(d => new google.maps.LatLng(d.lat, d.lng)),
+            // data: processedData,
+            map: map
+        });
     });
 }
 
 function appendMultiSelect(values) {
-    const multiSelect = $('select').selectize({
+    $('select').selectize({
         theme: 'links',
         plugins: ['remove_button', 'restore_on_backspace'],
         maxItems: null,
