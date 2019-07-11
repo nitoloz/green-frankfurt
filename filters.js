9fb0f3708db9df9c8f408c2871e6c6442a7852ca
Andrey <andrey.zolotin@gmail.com>
2019-07-11 17:22:42 +0200

    feat: split files by logic

--- filters.js
+++ filters.js
@@ -82,37 +82,37 @@ let filterType = FilterType.TREE_SPECIES;
 //     });
 // }
 //
-// function appendMultiSelect(values) {
-//     selectDropdown = $('select').selectize({
-//         theme: 'links',
-//         plugins: ['remove_button', 'restore_on_backspace'],
-//         maxItems: null,
-//         valueField: 'value',
-//         searchField: 'value',
-//         options: values,
-//         render: {
-//             option: function (data, escape) {
-//                 return `<div class="option">
-//                                 <span class="title">${escape(data.germanName)}</span>
-//                                 <span class="url"> (${escape(data.count)})</span>
-//                                 <div class="url">${escape(data.latinName)}</div>
-//                             </div>`;
-//             },
-//             item: function (data, escape) {
-//                 return `<div>${escape(data.germanName)} (${escape(data.count)})</div>`;
-//             }
-//         },
-//         onItemAdd: function (value, $item) {
-//             numberOfSelectedTrees = this.items.map(item => internalMap[item].count).reduce((a, b) => a + b);
-//             document.getElementById('show-selected').innerHTML = `Show ${numberOfSelectedTrees} trees!`;
-//         },
-//         onItemRemove: function (value, $item) {
-//             const countsArray = this.items.map(item => internalMap[item].count);
-//             numberOfSelectedTrees = countsArray.length > 0 ? countsArray.reduce((a, b) => a + b) : 0;
-//             document.getElementById('show-selected').innerHTML = `Show ${numberOfSelectedTrees} trees!`;
-//         }
-//     });
-// }
+function appendMultiSelect(values) {
+    selectDropdown = $('select').selectize({
+        theme: 'links',
+        plugins: ['remove_button', 'restore_on_backspace'],
+        maxItems: null,
+        valueField: 'value',
+        searchField: 'value',
+        options: values,
+        render: {
+            option: function (data, escape) {
+                return `<div class="option">
+                                <span class="title">${escape(data.germanName)}</span>
+                                <span class="url"> (${escape(data.count)})</span>
+                                <div class="url">${escape(data.latinName)}</div>
+                            </div>`;
+            },
+            item: function (data, escape) {
+                return `<div>${escape(data.germanName)} (${escape(data.count)})</div>`;
+            }
+        },
+        onItemAdd: function (value, $item) {
+            numberOfSelectedTrees = this.items.map(item => internalMap[item].count).reduce((a, b) => a + b);
+            document.getElementById('show-selected').innerHTML = `Show ${numberOfSelectedTrees} trees!`;
+        },
+        onItemRemove: function (value, $item) {
+            const countsArray = this.items.map(item => internalMap[item].count);
+            numberOfSelectedTrees = countsArray.length > 0 ? countsArray.reduce((a, b) => a + b) : 0;
+            document.getElementById('show-selected').innerHTML = `Show ${numberOfSelectedTrees} trees!`;
+        }
+    });
+}
 //
 // function filteredDataByTreeSpecies(selectedItems) {
 //     const filteredData = selectedItems
@@ -210,65 +210,65 @@ function changeFilters() {
 //     cluster.addMarkers(markers);
 // }
 //
-// function addTreeSpeciesBadges() {
-//     treeSpecies.forEach(species => {
-//         let filterSpan = document.createElement("span");
-//         filterSpan.className = 'badge badge-secondary filter-badge flex-grow-1';
-//         filterSpan.innerHTML = `${species}`;
-//         filterSpan.onclick = function () {
-//             const index = selectedTreeSpecies.indexOf(this.innerText);
-//             if (index === -1) {
-//                 selectedTreeSpecies.push(this.innerText);
-//                 this.className = this.className.replace('secondary', 'primary');
-//             } else {
-//                 selectedTreeSpecies.splice(index, 1);
-//                 this.className = this.className.replace('primary', 'secondary');
-//             }
-//             selectDropdown[0].selectize.setValue(availableTreeSpecies.filter(species => {
-//                 return selectedTreeSpecies.some(selectedSpecies => species.indexOf(selectedSpecies) !== -1);
-//             }));
-//         };
-//
-//         document.getElementById("species-badges").appendChild(filterSpan);
-//     })
-// }
-//
-// function addCityDistrictsBadges() {
-//     cityDistricts.forEach(disctrict => {
-//         let filterSpan = document.createElement("span");
-//         filterSpan.className = 'badge badge-secondary filter-badge flex-grow-1';
-//         filterSpan.innerHTML = `${disctrict}`;
-//         filterSpan.onclick = function () {
-//             const index = selectedCityDistricts.indexOf(this.innerText);
-//             // TODO use after geojson initialization!!!
-//             console.log(map.data.getFeatureById(this.innerText));
-//             const allLocalMultiPolys = [];
-//             const googleGeometryMultiPoly = [];
-//             map.data.getFeatureById(this.innerText).getGeometry().getArray().map((item, i) => {
-//                 allLocalMultiPolys[i] = [];
-//                 let curPolyNum = item.getLength();
-//                 for (let j = 0; j < curPolyNum; j++) {
-//                     allLocalMultiPolys[i].push(item.getAt(j).getArray());
-//                 }
-//                 googleGeometryMultiPoly.push(new google.maps.Polygon({
-//                     paths: allLocalMultiPolys[i]
-//                 }));
-//             });
-//             const filteredData = processedData.filter(d => {
-//                 const point = new google.maps.LatLng(d.lat, d.lng);
-//                 return googleGeometryMultiPoly.some(polygon => google.maps.geometry.poly.containsLocation(point, polygon));
-//             });
-//             numberOfSelectedTrees = filteredData.length;
-//             showFilteredDataPoints(filteredData);
-//             if (index === -1) {
-//                 selectedCityDistricts.push(this.innerText);
-//                 this.className = this.className.replace('secondary', 'primary');
-//             } else {
-//                 selectedCityDistricts.splice(index, 1);
-//                 this.className = this.className.replace('primary', 'secondary');
-//             }
-//         };
-//
-//         document.getElementById("district-badges").appendChild(filterSpan);
-//     })
-// }
+function addTreeSpeciesBadges() {
+    treeSpecies.forEach(species => {
+        let filterSpan = document.createElement("span");
+        filterSpan.className = 'badge badge-secondary filter-badge flex-grow-1';
+        filterSpan.innerHTML = `${species}`;
+        filterSpan.onclick = function () {
+            const index = selectedTreeSpecies.indexOf(this.innerText);
+            if (index === -1) {
+                selectedTreeSpecies.push(this.innerText);
+                this.className = this.className.replace('secondary', 'primary');
+            } else {
+                selectedTreeSpecies.splice(index, 1);
+                this.className = this.className.replace('primary', 'secondary');
+            }
+            selectDropdown[0].selectize.setValue(availableTreeSpecies.filter(species => {
+                return selectedTreeSpecies.some(selectedSpecies => species.indexOf(selectedSpecies) !== -1);
+            }));
+        };
+
+        document.getElementById("species-badges").appendChild(filterSpan);
+    })
+}
+
+function addCityDistrictsBadges() {
+    cityDistricts.forEach(disctrict => {
+        let filterSpan = document.createElement("span");
+        filterSpan.className = 'badge badge-secondary filter-badge flex-grow-1';
+        filterSpan.innerHTML = `${disctrict}`;
+        filterSpan.onclick = function () {
+            const index = selectedCityDistricts.indexOf(this.innerText);
+            // TODO use after geojson initialization!!!
+            console.log(map.data.getFeatureById(this.innerText));
+            const allLocalMultiPolys = [];
+            const googleGeometryMultiPoly = [];
+            map.data.getFeatureById(this.innerText).getGeometry().getArray().map((item, i) => {
+                allLocalMultiPolys[i] = [];
+                let curPolyNum = item.getLength();
+                for (let j = 0; j < curPolyNum; j++) {
+                    allLocalMultiPolys[i].push(item.getAt(j).getArray());
+                }
+                googleGeometryMultiPoly.push(new google.maps.Polygon({
+                    paths: allLocalMultiPolys[i]
+                }));
+            });
+            const filteredData = processedData.filter(d => {
+                const point = new google.maps.LatLng(d.lat, d.lng);
+                return googleGeometryMultiPoly.some(polygon => google.maps.geometry.poly.containsLocation(point, polygon));
+            });
+            numberOfSelectedTrees = filteredData.length;
+            showFilteredDataPoints(filteredData);
+            if (index === -1) {
+                selectedCityDistricts.push(this.innerText);
+                this.className = this.className.replace('secondary', 'primary');
+            } else {
+                selectedCityDistricts.splice(index, 1);
+                this.className = this.className.replace('primary', 'secondary');
+            }
+        };
+
+        document.getElementById("district-badges").appendChild(filterSpan);
+    })
+}
